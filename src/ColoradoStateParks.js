2851484192cbf82845adf08857c2af29db386b17
Faith <faithchemutai2330@gmail.com>
2023-01-15 15:11:59 +0300

    added files in colorado

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees,wildlife}from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(trees);
+  RMFunctions.wildlife();
 
-  return <h1>Colorado State Parks!</h1>;
+
+console.log(trees)
+wildlife();
+  return (
+    <div>
+    <MesaVerde/>
+  
+  </div>
+  );
 }
+export default ColoradoStateParks
