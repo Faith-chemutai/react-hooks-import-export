3c7728557aaa3a7ead1aad9a962a1f4734bda012
Faith <faithchemutai2330@gmail.com>
2023-01-15 16:16:56 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,22 +1,11 @@
+
+
 import React from "react";
 import howManyParks from "./parks/howManyParks";
-import MesaVerde from "./parks/MesaVerde";
-import {trees,wildlife}from "./parks/RockyMountain";
-import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-  console.log(trees);
-  RMFunctions.wildlife();
 
-
-console.log(trees)
-wildlife();
-  return (
-    <div>
-    <MesaVerde/>
-  
-  </div>
-  );
+  return <h1>Colorado State Parks!</h1>;
 }
 export default ColoradoStateParks
--- src/parks/RockyMountain.js
+++ src/parks/RockyMountain.js
@@ -1,9 +1,12 @@
-export const trees = "Aspen and Pine";
+const trees = "Aspen and Pine";
 
-export function wildlife() {
+function wildlife() {
   console.log("Elk, Bighorn Sheep, Moose");
 }
 
-function elevation() {
+function Elevation() {
   console.log("9583 ft");
+ 
 }
+
+export { trees, wildlife };
